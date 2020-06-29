9589f13fa8e00e7a83f13f861179c5bd5e81fb10
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-06-29 22:35:52 +0200

    Split velocity to 3 dimension vector.

--- Assets/Scripts/SpacecraftMock.cs
+++ Assets/Scripts/SpacecraftMock.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpacecraftMock : MonoBehaviour
 {
-    private const int VelocityMetersPerSecond = 10;
+    private const int VelocityMetersPerSecondX = 10;
+    private const int VelocityMetersPerSecondY = 10;
+    private const int VelocityMetersPerSecondZ = 10;
     private const bool Forward = true;
     private const int UnitLengthInMeters = 1000;
     private const float TranslationRate = 0.01f;
@@ -19,20 +20,7 @@ public class SpacecraftMock : MonoBehaviour
 
     private void Start()
     {
-        //StartCoroutine(MoveForward());
-    }
 
-    private IEnumerator MoveForward()
-    {
-        var transform = gameObject.transform;
-
-        while (true)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y,
-                transform.position.z + (Forward ? 1 : -1) * TranslationRate * VelocityMetersPerSecond / UnitLengthInMeters);
-
-            yield return new WaitForSeconds(TranslationRate);
-        }
     }
 
     private void Update()
@@ -72,12 +60,8 @@ public class SpacecraftMock : MonoBehaviour
     {
         if (horizontalInput != 0 || verticalInput != 0 || forwardInput != 0)
         {
-            transform.position += 
-                new Vector3(horizontalInput, verticalInput, forwardInput) * TranslationRate * VelocityMetersPerSecond / UnitLengthInMeters;
-
-            ////forwardInput = 0;
-            ////horizontalInput = 0;
-            ////verticalInput = 0;
+            transform.position +=
+                new Vector3(horizontalInput * VelocityMetersPerSecondX, verticalInput * VelocityMetersPerSecondY, forwardInput * VelocityMetersPerSecondZ) * TranslationRate / UnitLengthInMeters;
         }
     }
 }
