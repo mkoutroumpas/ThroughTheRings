83102c85c7ad9cfdc606eded673d7bee09a659ee
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-10-29 07:31:55 +0100

    Added MouseLookController and attached it to MainCamera.

---
+++ Assets/Scripts/MouseLookController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MouseLookController : MonoBehaviour
+{
+	Vector2 rotation = new Vector2 (0, 0);
+	public float speed = 3;
+
+	void Update () 
+    {
+		rotation.y += Input.GetAxis ("Mouse X");
+		rotation.x += -Input.GetAxis ("Mouse Y");
+		transform.eulerAngles = (Vector2)rotation * speed;
+	}
+}
+
