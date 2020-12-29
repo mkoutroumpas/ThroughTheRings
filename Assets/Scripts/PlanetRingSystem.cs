08e125ae876eb017a2e4f23542d3966ce43234f1
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-12-29 21:59:34 +0100

    Completed test ring system - code beautified (II).

--- Assets/Scripts/PlanetRingSystem.cs
+++ Assets/Scripts/PlanetRingSystem.cs
@@ -2,42 +2,33 @@ using UnityEngine;
 
 public class PlanetRingSystem : MonoBehaviour 
 {
-    private Vector3 CoordinateSystemZero; 
-    private float rA, rB, PlanetRadius = 30000f, RingRadius = 50000;
-    private const int numOfRingsBetween = 2, ringAngleStep = 2;
+    Vector3 coordinateSystemZero; 
+    float rA, rB, planetRadius = 30000f, ringRadius = 50000;
+    const int numOfRingsBetween = 2, ringAngleStep = 2;
 
     void Start() 
     {
-        CoordinateSystemZero = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-        PlanetRadius = gameObject.transform.localScale.z / 2;
+        coordinateSystemZero = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        planetRadius = gameObject.transform.localScale.z / 2;
 
-        rA = PlanetRadius + 10000;
-        rB = rA + RingRadius;
+        rA = planetRadius + 10000;
+        rB = rA + ringRadius;
 
-        Debug.Log($"CoordinateSystemZero = {CoordinateSystemZero}");
+        Debug.Log($"CoordinateSystemZero = {coordinateSystemZero}");
         Debug.Log($"rA = {rA}, rB = {rB}");
 
         AddTestRingSystem();
     }
 
-    private void AddTestCubes() 
+    void AddTestCube(int angle, float radius, float scale = 1000f) 
     {
-        for (int i = 0; i < 360; i += 5)
-        {
-            AddTestCube(i, rA, 100f);
-            AddTestCube(i, rB, 100f);
-        }
+        GameObject artifact = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        artifact.transform.localScale = new Vector3(scale, scale, scale); 
+        artifact.transform.position = new Vector3(radius * Mathf.Sin(angle * Mathf.PI / 180), 
+            coordinateSystemZero.y, coordinateSystemZero.z - radius * Mathf.Cos(angle * Mathf.PI / 180)); 
     }
 
-    private void AddTestCube(int angle, float radius, float scale = 1000f) 
-    {
-        GameObject cubeArtifact = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cubeArtifact.transform.localScale = new Vector3(scale, scale, scale); 
-        cubeArtifact.transform.position = new Vector3(radius * Mathf.Sin(angle * Mathf.PI / 180), 
-            CoordinateSystemZero.y, CoordinateSystemZero.z - radius * Mathf.Cos(angle * Mathf.PI / 180)); 
-    }
-
-    private void AddTestRingSystem()
+    void AddTestRingSystem()
     {
         for (int a = 0; a < 360; a += ringAngleStep) 
         {
@@ -45,5 +36,5 @@ public class PlanetRingSystem : MonoBehaviour
         }
     }
 
-    private float GetArtifactRadius(int ringId) => rA + ringId * RingRadius / (numOfRingsBetween + 1);
+    float GetArtifactRadius(int ringId) => rA + ringId * ringRadius / (numOfRingsBetween + 1);
 }
