13720494bcdddb9aea1aaf34504be140e95ec186
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-12-30 23:27:20 +0100

    Try to create ring system layers (I).

--- Assets/Scripts/PlanetRingSystem.cs
+++ Assets/Scripts/PlanetRingSystem.cs
@@ -1,15 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetRingSystem : MonoBehaviour 
 {
     Vector3 coordinateSystemZero; 
+    Dictionary<float, float> ringLayers;
     float rA, rB, planetRadius = 30000f, ringRadius = 50000;
-    const int numOfRingsBetween = 2, ringAngleStep = 2; 
+    const int numOfRingsBetween = 10, ringAngleStep = 2; 
     const int sizeAndDistanceMultiplier = 1; // Near-field objects scaling. 1: 1 unit corresponds to 10 m, 100: 1 unit corresponds to 1 km.
     const float testCubeScale = 100f;
 
     void Start() 
     {
+        ringLayers = new Dictionary<float, float>
+        {
+            {0f, 0f},
+            {10f, 1000f}
+        };
+
         coordinateSystemZero = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
         planetRadius = gameObject.transform.localScale.z / 2;
 
@@ -19,22 +27,25 @@ public class PlanetRingSystem : MonoBehaviour
         Debug.Log($"CoordinateSystemZero = {coordinateSystemZero}");
         Debug.Log($"rA = {rA}, rB = {rB}");
 
-        AddTestRingSystem();
+        foreach (var ringLayer in ringLayers)
+        {
+            AddTestRingSystem(ringLayer.Key, ringLayer.Value);
+        }
     }
 
-    void AddTestCube(int angle, float radius, float scale = 1000f) 
+    void AddTestCube(float angle, float radius, float scale = 1000f, float yOverhead = 0f) 
     {
         GameObject artifact = GameObject.CreatePrimitive(PrimitiveType.Cube);
         artifact.transform.localScale = new Vector3(scale, scale, scale); 
         artifact.transform.position = new Vector3(radius * Mathf.Sin(angle * Mathf.PI / 180), 
-            coordinateSystemZero.y, coordinateSystemZero.z - radius * Mathf.Cos(angle * Mathf.PI / 180)); 
+            coordinateSystemZero.y + yOverhead, coordinateSystemZero.z - radius * Mathf.Cos(angle * Mathf.PI / 180)); 
     }
 
-    void AddTestRingSystem()
+    void AddTestRingSystem(float startAngle = 0f, float yOverhead = 0f)
     {
         for (int a = 0; a < 360; a += ringAngleStep) 
         {
-            for (int i = 0; i <= numOfRingsBetween + 1; i++) AddTestCube(a, GetArtifactRadius(i), testCubeScale);
+            for (int i = 0; i <= numOfRingsBetween + 1; i++) AddTestCube(a + startAngle, GetArtifactRadius(i), testCubeScale, yOverhead);
         }
     }
 
