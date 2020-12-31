6dac0f9df249d265caf6a08d56f3124ca58a4c72
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-12-31 15:00:04 +0100

    Randomize test artifact size (I).

--- Assets/Scripts/PlanetRingSystem.cs
+++ Assets/Scripts/PlanetRingSystem.cs
@@ -7,16 +7,18 @@ public class PlanetRingSystem : MonoBehaviour
     List<(float Angle, float YOverhead, Color Color)> ringLayers;
     float rA, rB, planetRadius = 30000f, ringRadius = 50000;
     const int numOfRingsBetween = 10, ringAngleStep = 6;
-    const int sizeAndDistanceMultiplier = 1; // Near-field objects scaling. 1: 1 unit corresponds to 10 m, 100: 1 unit corresponds to 1 km.
-    const float testCubeScale = 250f;
+    const int sizeAndDistanceMultiplier = 1; // 1: a unit corresponds to 10 m (near-field objects scaling), 100: a unit corresponds to 1 km (far-field objects scaling).
+    const float uniformTestCubeScale = 250f;
+    float minCubeScale = 100f, maxCubeScale = 1000f;
+    static System.Random random;
 
     void Start() 
     {
         ringLayers = new List<(float, float, Color)>
         {
-            (0f, -1000f, Color.green),
+            (0f, -2000f, Color.green),
             (2f, 0f, Color.red),
-            (4f, 1000f, Color.blue)
+            (4f, 2000f, Color.blue)
         };
 
         coordinateSystemZero = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
@@ -28,14 +30,19 @@ public class PlanetRingSystem : MonoBehaviour
         Debug.Log($"CoordinateSystemZero = {coordinateSystemZero}");
         Debug.Log($"rA = {rA}, rB = {rB}");
 
-        foreach (var ringLayer in ringLayers) AddTestRingSystem(ringLayer.Angle, ringLayer.YOverhead, ringLayer.Color);
+        foreach (var ringLayer in ringLayers) AddTestRingSystem(ringLayer.Angle, ringLayer.YOverhead, ringLayer.Color, true);
     }
 
-    void AddTestRingSystem(float startAngle = 0f, float yOverhead = 0f, Color color = default)
+    void AddTestRingSystem(float startAngle = 0f, float yOverhead = 0f, Color color = default, bool randomize = false)
     {
         for (int a = 0; a < 360; a += ringAngleStep) 
         {
-            for (int i = 0; i <= numOfRingsBetween + 1; i++) AddTestCube(a + startAngle, GetArtifactRadialDistance(i), testCubeScale, yOverhead, color);
+            for (int i = 0; i <= numOfRingsBetween + 1; i++) 
+            {
+                float scale = randomize ? GetRandomArtifactSize(minCubeScale, maxCubeScale) : uniformTestCubeScale;
+                
+                AddTestCube(a + startAngle, GetArtifactRadialDistance(i), scale, yOverhead, color);
+            }
         }
     }
 
@@ -50,4 +57,10 @@ public class PlanetRingSystem : MonoBehaviour
     }
 
     float GetArtifactRadialDistance(int ringId) => rA + ringId * ringRadius * sizeAndDistanceMultiplier / (numOfRingsBetween + 1);
+
+    float GetRandomArtifactSize(float minSize = 1f, float maxSize = 1000f) 
+    {
+        if (random == null) random = new System.Random();
+        return (float)(random.NextDouble() * (maxSize - minSize) + minSize);
+    }
 }
