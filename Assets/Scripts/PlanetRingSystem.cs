9b711cece9934ab70e85a1dcc1466f3bad2f0c2e
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-01-02 22:32:36 +0100

    Try to apply Normal distribution (I).

--- Assets/Scripts/PlanetRingSystem.cs
+++ Assets/Scripts/PlanetRingSystem.cs
@@ -7,7 +7,7 @@ public class PlanetRingSystem : MonoBehaviour
     List<(float Angle, float YOverhead, Color Color)> ringLayers;
     float rA, rB, planetRadius = 30000f, ringRadius = 50000;
     const int numOfRingsBetween = 20, ringAngleStep = 3;
-    const int stdDeviation = 1;
+    const int stdDeviation = 100;
     const int sizeAndDistanceMultiplier = 1; // 1: a unit corresponds to 10 m (near-field objects scaling), 100: a unit corresponds to 1 km (far-field objects scaling).
     const float uniformTestCubeScale = 250f;
     float minCubeScale = 0.01f, maxCubeScale = 1000f;
@@ -48,7 +48,7 @@ public class PlanetRingSystem : MonoBehaviour
         {
             for (int i = 0; i <= numOfRingsBetween + 1; i++) 
             {
-                float scale = randomize ? GetArtifactSize(minCubeScale, maxCubeScale) : uniformTestCubeScale;
+                float scale = randomize ? GetArtifactSize(minCubeScale, maxCubeScale, Distributions.Gaussian) : uniformTestCubeScale;
 
                 AddTestCube(a + startAngle, GetArtifactRadialDistance(i), scale, yOverhead, color);
             }
