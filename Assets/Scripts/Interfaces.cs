56f286870802cf1c342771c5b21fd8e010c2d3bf
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-01-05 21:49:27 +0100

    Start adding support for artifact abstraction (II).

--- Assets/Scripts/Interfaces.cs
+++ Assets/Scripts/Interfaces.cs
@@ -1,4 +1,9 @@
+using UnityEngine;
+
 public interface IRingObject
 {
-    
+    Vector3 Rotation { get; }
+    Vector3 Position { get; }
+
+    void SetPosition(float radialDistance, float angle);
 }
--- Assets/Scripts/RingObject.cs
+++ Assets/Scripts/RingObject.cs
@@ -1,4 +1,11 @@
+using UnityEngine;
 public class RingObject : IRingObject
 {
+    public Vector3 Position { get; set; }
+    public Vector3 Rotation { get; set; }
 
+    public void SetPosition(float radialDistance, float angle)
+    {
+        // TODO: Trigonometrically set position coordinates.
+    }
 }
