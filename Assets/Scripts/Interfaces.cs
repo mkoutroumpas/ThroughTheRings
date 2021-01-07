9cc1d27b2d5faea7fcab7800e50a52f59c4143b6
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-01-07 19:19:28 +0100

    Add RotationSpeed to IRingObject.

--- Assets/Scripts/Interfaces.cs
+++ Assets/Scripts/Interfaces.cs
@@ -4,6 +4,7 @@ public interface IRingObject
 {
     Vector3 Rotation { get; }
     Vector3 Position { get; }
+    Vector3 RotationSpeed { get; }
 
     void SetPosition(float radialDistance, float angle);
 
