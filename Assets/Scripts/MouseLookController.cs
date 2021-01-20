5bc096faf0efd59972239cb931c76a081574e4f7
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-01-20 21:42:52 +0100

    Clean up typos.

--- Assets/Scripts/MouseLookController.cs
+++ Assets/Scripts/MouseLookController.cs
@@ -6,8 +6,8 @@ public class MouseLookController : MonoBehaviour
 	float speed = 3;
 	void Update () 
     {
-		rotation.y += Input.GetAxis ("Mouse X");
-		rotation.x += -Input.GetAxis ("Mouse Y");
+		rotation.y += Input.GetAxis("Mouse X");
+		rotation.x += -Input.GetAxis("Mouse Y");
 		transform.eulerAngles = (Vector2)rotation * speed;
 	}
 }
