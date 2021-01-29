6952a7ebe3c2ac7bbcedee2f72db40ab53099064
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-01-29 21:23:46 +0100

    Try and find out why SystemRotation won't work for many objects (I) - add Log().

--- Assets/Scripts/PlanetRingSystem.cs
+++ Assets/Scripts/PlanetRingSystem.cs
@@ -62,8 +62,10 @@ public class PlanetRingSystem : MonoBehaviour
 
                 var R = Mathf.Abs(rOTransform.position.z - this._coordinateSystemZero.z);
 
-                rOTransform.position = 
-                    new Vector3(Mathf.Cos(this._timeCounter) * R, 0f, Mathf.Sin(this._timeCounter) * R + this._coordinateSystemZero.z);
+                Debug.Log($"R: {R}");
+
+                // rOTransform.position = 
+                //     new Vector3(Mathf.Cos(this._timeCounter) * R, 0f, Mathf.Sin(this._timeCounter) * R + this._coordinateSystemZero.z);
 
                 rOTransform.Rotate(ringObject.SelfRotationSpeed * Time.deltaTime, Space.Self);
             }
