2e2656e0a4ebcbdaea245af42c09f52f9b8b3300
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-02-05 22:41:17 +0100

    SetRadius() to ringObject.

--- Assets/Scripts/PlanetRingSystem.cs
+++ Assets/Scripts/PlanetRingSystem.cs
@@ -199,6 +199,7 @@ public class PlanetRingSystem : MonoBehaviour
         RingObject ringObject = new RingObject(new Vector3(xPos, yPos, zPos), this._coordinateSystemZero, PrimitiveType.Cube, scale);
         ringObject.SetColor(color);
         ringObject.SetAngle(angle);
+        ringObject.SetRadius(radius);
         ringObject.SetSelfRotationSpeed(
             new Vector3(
                 (float)(random.NextDouble() * this._diffSelfRotSpeed + this._minSelfRotSpeed),
