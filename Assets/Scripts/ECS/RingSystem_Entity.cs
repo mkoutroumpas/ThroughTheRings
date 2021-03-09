6d0e71f21607f0dd4015b76b9cdc1cccb2ba394b
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-03-09 18:35:07 +0100

    Add RingSystem_Entity.

---
+++ Assets/Scripts/ECS/RingSystem_Entity.cs
@@ -0,0 +1,10 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class RingSystem_Entity : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager manager, GameObjectConversionSystem conversionSystem)
+    {
+        
+    }
+}
