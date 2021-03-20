37280f3e6d0cbb85cd2cf54f2fd280c5988ca97f
Marios Koutroumpas <mku@trackman.com>
2021-03-20 22:53:07 +0100

    Initialize RingObject_Appearance.

--- Assets/Scripts/ECS/RingSystem_Entity.cs
+++ Assets/Scripts/ECS/RingSystem_Entity.cs
@@ -5,7 +5,7 @@ public class RingSystem_Entity : MonoBehaviour, IConvertGameObjectToEntity
 {
     public void Convert(Entity entity, EntityManager manager, GameObjectConversionSystem conversionSystem)
     {
-        RingObject_Appearance appearance = new RingObject_Appearance();
+        RingObject_Appearance appearance = new RingObject_Appearance { Color = Color.gray, PrimitiveType = PrimitiveType.Cube, Scale = new Vector3(1f, 1f, 1f) };
         manager.AddComponentData(entity, appearance);
 
         RingObject_Position position = new RingObject_Position();
