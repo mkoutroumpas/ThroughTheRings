18133984b0ed0a3fe3c93e360a4511605474fa0d
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-04-04 13:29:11 +0200

    Assign const values.

--- Assets/Scripts/ECS/Components/RingObject_SystemConstants.cs
+++ Assets/Scripts/ECS/Components/RingObject_SystemConstants.cs
@@ -7,11 +7,12 @@ public struct RingObject_SystemConstants : IComponentData
 {
     public Vector3 CoordinateSystemZero;
     public float PlanetRadius;
-    public float RingWidth;
-    public int NumOfRingsAB;
-    public int RingAngleStep;
-    public float MinRingObjectScale, MaxRingObjectScale;
-    public float MinYDeviation, MaxYDeviation;
-    public bool EnableRingObjectsRotation;
-    public FieldDepths FieldDepth;
+    public const float RingWidth = 50000.0f;
+    public const int NumOfRingsAB = 20;
+    public const int RingAngleStep = 3;
+    public const float MinRingObjectScale =  0.001f, MaxRingObjectScale = 250.0f;
+    public const float MinDeviation = -5000.0f, MaxDeviation = 5000.0f;
+    public const float MinYDeviation = -500.0f, MaxYDeviation = 500.0f;
+    public const bool EnableRingObjectsRotation = true;
+    public const FieldDepths FieldDepth = FieldDepths.Near;
 }
