87a6d1a8defdcaae918b92cc6487c09f79a84e20
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-04-06 22:50:32 +0200

    1. Rename to RingSystem_Playback, 2. Add more system overrides and ringObjectQuery.

---
+++ Assets/Scripts/ECS/RingSystem_Playback.cs
@@ -0,0 +1,19 @@
+using Unity.Entities;
+
+public class RingSystem_Playback : SystemBase
+{
+    #region Private variables
+    EntityQuery ringObjectQuery;
+    #endregion
+
+    #region System overrides
+    protected override void OnCreate()
+    {
+        ringObjectQuery = GetEntityQuery(typeof(RingObject_RotationSpeed), typeof(RingObject_Appearance), typeof(RingObject_Position), typeof(RingObject_SystemData));
+    }
+    protected override void OnUpdate()
+    {
+
+    }
+    #endregion
+}
