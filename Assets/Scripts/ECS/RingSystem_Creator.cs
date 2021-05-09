6b54d854f58427dd1c2206bdd0c78717c900a27e
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-05-09 13:29:11 +0200

    Move initialization to EntitySpawner (IV).

--- Assets/Scripts/ECS/RingSystem_Creator.cs
+++ Assets/Scripts/ECS/RingSystem_Creator.cs
@@ -6,20 +6,12 @@ using Unity.Collections;
 public class RingSystem_Creator : SystemBase
 {
     #region Constants
-    const float RingWidth = 50000.0f;
-    const int NumOfRingsAB = 20;
-    const int RingAngleStep = 3;
-    const float StdDeviation = 0.1f;
     const int SystemAngleSpanDegs = 360;
-    const float MinRingObjectScale =  0.001f, MaxRingObjectScale = 250.0f;
-    const float MinDeviation = -5000.0f, MaxDeviation = 5000.0f;
-    const float MinYDeviation = -500.0f, MaxYDeviation = 500.0f;
     const float MaxSelfRotationSpeed = 10.0f, MinSelfRotationSpeed = 0.0f;
     const float DiffSelfRotSpeed = MaxSelfRotationSpeed - MinSelfRotationSpeed;
     const float MaxSystemRotationSpeed = 0.005f, MinSystemRotationSpeed = 0.0025f;
     const float DiffSystemRotSpeed = MaxSystemRotationSpeed - MinSystemRotationSpeed;
     const bool EnableRingObjectsRotation = true;
-    const FieldDepths FieldDepth = FieldDepths.Near;
     const Distributions Distribution = Distributions.White;
     #endregion
 
