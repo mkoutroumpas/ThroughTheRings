14daca3f5cd4f52266e59f4dabf8eb6bbf459d19
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-05-09 12:54:39 +0200

    EntityConverter doesn't need to implement IDeclareReferencedPrefabs anymore.

--- Assets/Scripts/ECS/RingSystem_EntityConverter.cs
+++ Assets/Scripts/ECS/RingSystem_EntityConverter.cs
@@ -1,16 +1,8 @@
-using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
-using Unity.Transforms;
 
-public class RingSystem_EntityConverter : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+public class RingSystem_EntityConverter : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public GameObject RingObjectPrefab;
-
-    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
-    {
-        referencedPrefabs.Add(RingObjectPrefab);
-    }
     public void Convert(Entity entity, EntityManager manager, GameObjectConversionSystem conversionSystem)
     {
         RingObject_Appearance appearance = new RingObject_Appearance { Color = Color.gray, Scale = new Vector3(1f, 1f, 1f) };
@@ -27,8 +19,7 @@ public class RingSystem_EntityConverter : MonoBehaviour, IDeclareReferencedPrefa
         RingObject_SystemData systemData = new RingObject_SystemData 
         { 
             CoordinateSystemZero = new Vector3(gOT.position.x, gOT.position.y, gOT.position.z), 
-            PlanetRadius = gOT.localScale.z / 2, 
-            Entity = conversionSystem.GetPrimaryEntity(RingObjectPrefab)
+            PlanetRadius = gOT.localScale.z / 2
         };
         manager.AddComponentData(entity, systemData);
     }
