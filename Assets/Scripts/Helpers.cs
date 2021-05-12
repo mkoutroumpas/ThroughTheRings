c1093c2d784136dc7760518bdfbb3c5e7ab5ed06
Marios Koutroumpas <mkoutroumpas@gmail.com>
2021-05-12 06:44:37 +0200

    Add Settings class.

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -5,3 +5,8 @@ public static class Helpers
     public static Vector3 ToCartesian(float angle, float radius, float yOverhead, Vector3 coordinateSystemZero) => 
         new Vector3(radius * Mathf.Cos(angle) + coordinateSystemZero.x, yOverhead, radius * Mathf.Sin(angle) + coordinateSystemZero.z);
 }
+
+public class Settings
+{
+
+}
