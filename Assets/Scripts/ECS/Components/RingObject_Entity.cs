4549babfce0ea989189a8dc7d86d7c6d8bf290e4
mkoutroumpas <mkoutroumpas@gmail.com>
2022-02-13 09:37:43 +0100

    Add RingObject_Entity component.

---
+++ Assets/Scripts/ECS/Components/RingObject_Entity.cs
@@ -0,0 +1,6 @@
+using System;
+using UnityEngine;
+using Unity.Entities;
+
+[Serializable]
+public struct RingObject_Entity : IComponentData {  }
--- Assets/Scripts/ECS/RingSystem_EntityConverter.cs
+++ Assets/Scripts/ECS/RingSystem_EntityConverter.cs
@@ -5,13 +5,15 @@ public class RingSystem_EntityConverter : MonoBehaviour, IConvertGameObjectToEnt
 {
     public void Convert(Entity entity, EntityManager manager, GameObjectConversionSystem conversionSystem)
     {
+        manager.AddComponent(entity, typeof(RingObject_Entity));
+        
         RingObject_Appearance appearance = new RingObject_Appearance { Color = Color.gray, Scale = new Vector3(1f, 1f, 1f) };
         manager.AddComponentData(entity, appearance);
 
         RingObject_Position position = new RingObject_Position { Angle = 0f, Radius = 10000f };
         manager.AddComponentData(entity, position);
 
-        RingObject_RotationSpeed rorationSpeed = new RingObject_RotationSpeed { SelfRotationSpeed = new Vector3(1f, 1f, 1f), SystemRotationSpeed = new Vector3(1f, 1f, 1f) };
+        RingObject_RotationSpeed rorationSpeed = new RingObject_RotationSpeed { Self = new Vector3(1f, 1f, 1f), System = new Vector3(1f, 1f, 1f) };
         manager.AddComponentData(entity, rorationSpeed);
     }
 }
