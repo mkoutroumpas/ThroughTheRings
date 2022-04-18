d083f5d7d304332139dd8cd4e3c3f43311cfd90f
mkoutroumpas <mkoutroumpas@gmail.com>
2022-04-18 08:25:01 +0200

    Support multiple rotation axes.

--- Assets/Scripts/ECS/RingSystem_System.cs
+++ Assets/Scripts/ECS/RingSystem_System.cs
@@ -15,9 +15,16 @@ public class RingSystem_System : SystemBase
             .WithName("RingSystem_System")
             .ForEach((ref Rotation rotation, in RingObject_RotationSpeed rotationSpeed) =>
             {
+                float rA = rotationSpeed.Self.x;
+                float3 rV = math.right();
+
+                if (rotationSpeed.Self.y != 0) { rA = rotationSpeed.Self.y; rV = math.up(); }
+                if (rotationSpeed.Self.z != 0) { rA = rotationSpeed.Self.z; rV = math.forward(); }
+
                 rotation.Value = math.mul(
                     math.normalize(rotation.Value),
-                    quaternion.AxisAngle(math.up(), rotationSpeed.Self.y * deltaTime));
+                    quaternion.AxisAngle(rV, rA * deltaTime));
+
             }).ScheduleParallel();
     }
 }
