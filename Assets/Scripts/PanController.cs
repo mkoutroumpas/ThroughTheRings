6de1655f49d82e2d0ebc1cbf9ad5d79f0c9c8a6c
mkoutroumpas <mkoutroumpas@gmail.com>
2022-05-17 21:28:33 +0200

    Add panning controller and WASD controls.

---
+++ Assets/Scripts/PanController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PanController : MonoBehaviour
+{
+	void Update () 
+    {
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			rigidbody.AddForce(Vector3.forward);
+			return;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			rigidbody.AddForce(Vector3.back);
+			return;
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			rigidbody.AddForce(Vector3.left);
+			return;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			rigidbody.AddForce(Vector3.right);
+			return;
+		}	
+	}
+}
+
