b79d35daa7f1ac3ac3cc7d3b51c65b48e3870c1e
mkoutroumpas <mkoutroumpas@gmail.com>
2022-05-26 09:08:27 +0200

    Switch to KeyDown.

--- Assets/Scripts/PanController.cs
+++ Assets/Scripts/PanController.cs
@@ -9,25 +9,10 @@ public class PanController : MonoBehaviour
 	}
 	void Update () 
     {
-		if (Input.GetKeyDown(KeyCode.W))
-		{
-			transform.position += Vector3.forward * speed * Time.deltaTime;
-		}
-
-		if (Input.GetKeyDown(KeyCode.S))
-		{
-			transform.position += Vector3.back* speed * Time.deltaTime;
-		}
-
-		if (Input.GetKeyDown(KeyCode.A))
-		{
-			transform.position += Vector3.left* speed * Time.deltaTime;
-		}
-
-		if (Input.GetKeyDown(KeyCode.D))
-		{
-			transform.position += Vector3.right * speed * Time.deltaTime;
-		}	
+		if (Input.GetKeyDown(KeyCode.W)) transform.position += Vector3.forward * speed * Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.S)) transform.position += Vector3.back* speed * Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.A)) transform.position += Vector3.left* speed * Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.D)) transform.position += Vector3.right * speed * Time.deltaTime;
 	}
 }
 
